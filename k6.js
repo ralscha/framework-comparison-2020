9605ac12206abd59cc2084393b7ccb70a83e73d7
Ralph Schaer <ralphschaer@gmail.com>
2020-06-28 10:37:39 +0200

    Update

--- k6.js
+++ k6.js
@@ -3,11 +3,11 @@ import { check, sleep } from "k6";
 
 export const options = {
     stages: [
-        // Linearly ramp up from 1 to 500 VUs during first minute
-        { target: 500, duration: "1m" },
-        // Hold at 500 VUs for the next 3 minutes and 30 seconds
-        { target: 500, duration: "3m30s" },
-        // Linearly ramp down from 500 to 0 VUs over the last 30 seconds
+        // Linearly ramp up from 1 to 1000 VUs during first minute
+        { target: 1000, duration: "1m" },
+        // Hold at 1000 VUs for the next 3 minutes and 30 seconds
+        { target: 1000, duration: "3m30s" },
+        // Linearly ramp down from 1000 to 0 VUs over the last 30 seconds
         { target: 0, duration: "30s" }
         // Total execution time will be ~5 minutes
     ]
@@ -21,5 +21,5 @@ export default function() {
         "content is present": (r) => r.body.indexOf("{\"msg\":\"Hello John\",\"ts\"") !== -1,
     });
 		
-	sleep(Math.random() * 3 + 2); // Random sleep between 2s and 5s
+	sleep(Math.random() * 3); // Random sleep between 0s and 3s
 };
