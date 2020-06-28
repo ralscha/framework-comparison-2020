bb752db7797e30f2780a0ecd4945b5c7bab39c15
Ralph Schaer <ralphschaer@gmail.com>
2020-06-28 10:29:26 +0200

    Update

--- k6.js
+++ k6.js
@@ -1,34 +1,25 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { Rate } from "k6/metrics";
-
-var failureRate = new Rate("check_failure_rate");
 
 export const options = {
-	  vus: 500,
-		duration: '30s'
-		/*
     stages: [
-        // Linearly ramp up from 1 to 200 VUs during first minute
-        { target: 200, duration: "1m" },
-        // Hold at 200 VUs for the next 3 minutes and 30 seconds
-        { target: 200, duration: "3m30s" },
-        // Linearly ramp down from 200 to 0 VUs over the last 30 seconds
+        // Linearly ramp up from 1 to 1000 VUs during first minute
+        { target: 1000, duration: "1m" },
+        // Hold at 1000 VUs for the next 3 minutes and 30 seconds
+        { target: 1000, duration: "3m30s" },
+        // Linearly ramp down from 1000 to 0 VUs over the last 30 seconds
         { target: 0, duration: "30s" }
         // Total execution time will be ~5 minutes
-    ],
-		*/
+    ]
 };
 
 export default function() {
     const response = http.get("http://localhost:8080/hello/John");
 		
-		 const checkRes = check(response, {
+    const checkRes = check(response, {
         "status is 200": (r) => r.status === 200,
-        "content is present": (r) => r.body.indexOf("Hello John") !== -1,
+        "content is present": (r) => r.body.indexOf("{\"msg\":\"Hello John\",\"ts\"") !== -1,
     });
-
-    failureRate.add(!checkRes);
 		
-		// sleep(Math.random() * 3 + 2); // Random sleep between 2s and 5s
+	sleep(Math.random() * 3 + 2); // Random sleep between 2s and 5s
 };
