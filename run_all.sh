f3ebb5cee9fb451260633206ce5190c9ed243001
Ralph Schaer <ralphschaer@gmail.com>
2020-12-30 18:41:04 +0100

    Polish

--- run_all.sh
+++ run_all.sh
@@ -16,6 +16,10 @@ sleep 5
 npm run time
 sleep 10
 
+export JAVA_OPTS_MICRONAUT="-XX:SharedArchiveFile=openjdk-app-micronaut.jar.jsa"
+export JAVA_OPTS_QUARKUS="-XX:SharedArchiveFile=openjdk-app-quarkus-runner.jar.jsa"
+export JAVA_OPTS_SPRING="-XX:SharedArchiveFile=openjdk-app-springboot.jar.jsa"
+export JAVA_VM="openjdk"
 source ./run_k6_java.sh
 source ./run_k6_native.sh
 
@@ -32,4 +36,8 @@ sleep 5
 npm run time
 sleep 10
 
+export JAVA_OPTS_MICRONAUT="-Xshareclasses:name=micronaut.jar"
+export JAVA_OPTS_QUARKUS="-Xshareclasses:name=quarkus-runner.jar"
+export JAVA_OPTS_SPRING="-Xshareclasses:name=springboot.jar"
+export JAVA_VM="openj9"
 source ./run_k6_java.sh
--- run_k6_java.sh
+++ run_k6_java.sh
@@ -1,7 +1,7 @@
-java -jar micronaut.jar &
-./k6 run --summary-export=micronaut1.json k6.js
-./k6 run --summary-export=micronaut2.json k6.js
-./k6 run --summary-export=micronaut3.json k6.js
+java $JAVA_OPTS_MICRONAUT -jar micronaut.jar &
+./k6 run --summary-export=micronaut1-$JAVA_VM.json k6.js
+./k6 run --summary-export=micronaut2-$JAVA_VM.json k6.js
+./k6 run --summary-export=micronaut3-$JAVA_VM.json k6.js
 ps x -o rss,vsz,command | grep java
 sleep 5
 ps x -o rss,vsz,command | grep java
@@ -10,10 +10,10 @@ ps x -o rss,vsz,command | grep java
 pkill java
 sleep 30
 
-java -jar quarkus-runner.jar &
-./k6 run --summary-export=quarkus1.json k6.js
-./k6 run --summary-export=quarkus2.json k6.js
-./k6 run --summary-export=quarkus3.json k6.js
+java $JAVA_OPTS_QUARKUS -jar quarkus-runner.jar &
+./k6 run --summary-export=quarkus1-$JAVA_VM.json k6.js
+./k6 run --summary-export=quarkus2-$JAVA_VM.json k6.js
+./k6 run --summary-export=quarkus3-$JAVA_VM.json k6.js
 ps x -o rss,vsz,command | grep java
 sleep 5
 ps x -o rss,vsz,command | grep java
@@ -23,10 +23,10 @@ pkill java
 sleep 30
 
 
-java -jar springboot.jar &
-./k6 run --summary-export=springboot1.json k6.js
-./k6 run --summary-export=springboot2.json k6.js
-./k6 run --summary-export=springboot3.json k6.js
+java $JAVA_OPTS_SPRING -jar springboot.jar &
+./k6 run --summary-export=springboot1-$JAVA_VM.json k6.js
+./k6 run --summary-export=springboot2-$JAVA_VM.json k6.js
+./k6 run --summary-export=springboot3-$JAVA_VM.json k6.js
 ps x -o rss,vsz,command | grep java
 sleep 5
 ps x -o rss,vsz,command | grep java
