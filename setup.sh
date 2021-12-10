1b4b2d06091aa953c4c032d28fb4908e4a39d8b7
Ralph Schaer <ralphschaer@gmail.com>
2021-12-10 18:24:21 +0100

    Auto install

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
 
 
 git clone https://github.com/ralscha/framework-comparison-2020.git
