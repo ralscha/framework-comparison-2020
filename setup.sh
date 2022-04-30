5f44c3bdb67b528e10399d7a058b963e22c60dc9
Ralph Schaer <ralphschaer@gmail.com>
2022-04-30 17:36:15 +0200

    Upgrade

--- setup.sh
+++ setup.sh
@@ -2,30 +2,28 @@ apt update
 apt dist-upgrade -y
 apt-get remove docker docker-engine docker.io containerd runc -y
 apt install git screen apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce -y
 
 git clone https://github.com/ralscha/framework-comparison-2020.git
 cd framework-comparison-2020
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz
-tar xzf OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz
-rm OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz
-mv jdk-17.0.2+8 openjdk
+wget https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.1%2B10/OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz
+tar xzf OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz
+rm OpenJDK18U-jdk_x64_linux_hotspot_18.0.1_10.tar.gz
+mv jdk-18.0.1+10 openjdk
 
-wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.0.0.2/graalvm-ce-java17-linux-amd64-22.0.0.2.tar.gz
-tar xzf graalvm-ce-java17-linux-amd64-22.0.0.2.tar.gz
-rm graalvm-ce-java17-linux-amd64-22.0.0.2.tar.gz
+wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.1.0/graalvm-ce-java17-linux-amd64-22.1.0.tar.gz
+tar xzf graalvm-ce-java17-linux-amd64-22.1.0.tar.gz
+rm graalvm-ce-java17-linux-amd64-22.1.0.tar.gz
 apt-get install build-essential libz-dev zlib1g-dev -y
-mv graalvm-ce-java17-22.0.0.2 graal
+mv graalvm-ce-java17-22.1.0 graal
 export JAVA_HOME=`pwd`/graal
 PATH=$JAVA_HOME/bin:$PATH
 gu install native-image
@@ -35,9 +33,9 @@ tar xzf k6-v0.37.0-linux-amd64.tar.gz
 mv k6-v0.37.0-linux-amd64/k6 .
 rm -fr k6-*
 
-wget https://go.dev/dl/go1.18.linux-amd64.tar.gz
-tar xzf go1.18.linux-amd64.tar.gz
-rm go1.18.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+tar xzf go1.18.1.linux-amd64.tar.gz
+rm go1.18.1.linux-amd64.tar.gz
 
 npm install
 
